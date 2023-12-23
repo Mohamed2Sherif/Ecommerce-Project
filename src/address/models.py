a328a184bca5b17889bdb15383d934b23236bb2b
Mohamed2Sherif <91475739+Mohamed2Sherif@users.noreply.github.com>
2023-12-24 00:49:41 +0200

    modify address model and change admin panel

--- src/address/models.py
+++ src/address/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 import uuid
 from src.users.models import User
-
 class AddressTypeChoices(models.TextChoices):
     BILLING = 'billing', 'Billing'
     HOME = 'home', 'Home'
@@ -9,8 +8,8 @@ class AddressTypeChoices(models.TextChoices):
 # Create your models here.
 class Address(models.Model):
     address_ID = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
-    user_ID = models.ForeignKey(User,on_delete=models.CASCADE)
-    effective_Date = models.DateField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    effective_date = models.DateField()
     address_type = models.CharField(max_length=20, choices=AddressTypeChoices.choices)
     address_line_1 = models.CharField(max_length=255)
     address_line_2 = models.CharField(max_length=255, blank=True, null=True)
