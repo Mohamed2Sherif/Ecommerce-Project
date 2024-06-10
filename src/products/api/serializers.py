d29e7101c0ee7c40e39c06f922cc9c0aa5eca598
Mohamed2Sherif <91475739+Mohamed2Sherif@users.noreply.github.com>
2024-06-10 14:01:31 +0300

    feat/make the product AvailableSizes MultipleChoices Serializer

--- src/products/api/serializers.py
+++ src/products/api/serializers.py
@@ -1,27 +1,25 @@
 from rest_framework import serializers
 from src.products.models import Product
+from src.orders.utils import SizeChoices
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(read_only=True)
-
-    def validate_product_info(self, value):
-        if "name" not in value:
-            raise serializers.ValidationError(
-                "Product_info must contain a name attribute"
-            )
-        if "price" not in value:
-            raise serializers.ValidationError(
-                "product information must contain a price attribute"
-            )
-        if "qty" not in value:
-            raise serializers.ValidationError(
-                "product information must contain a quantity attribute"
-            )
-
-        return value
+    sizes = serializers.ListField(
+        child=serializers.MultipleChoiceField(choices=SizeChoices.CHOICES)
+    )
 
     class Meta:
         model = Product
         fields = ["id", "product_info", "category_info"]
         read_only_fields = ["created_time", "id", "updated_time"]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["sizes"] = instance.sizes.split(",")
+        return representation
+
+    def to_internal_value(self, data):
+        internal_value = super().to_internal_value(data)
+        sizes = data.get("sizes", [])
+        internal_value["sizes"] = ",".join(sizes)
+        return internal_value
