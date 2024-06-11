d303facf27cd1d4b444390586cc63e734979bf9d
Mohamed2Sherif <91475739+Mohamed2Sherif@users.noreply.github.com>
2024-06-11 16:36:04 +0300

    feat:Add Caching to OrderService

--- src/orders/Services/OrderService.py
+++ src/orders/Services/OrderService.py
@@ -2,37 +2,64 @@ from src.orders.Services.contracts.IOrderService import IOrderService
 from src.orders.api.serializers import OrderSerializer
 from src.orders.models import Order
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.cache import cache
 
 
 class OrderService(IOrderService):
     def createOrder(self, order_data):
-        serializer = OrderSerializer(data=order_data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            data = serializer.data
-            return data
+        try:
+            serializer = OrderSerializer(data=order_data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                data = serializer.data
+                cache.set("order_{data.order_id}", data)
+                return data
+        except Exception as e:
+            raise Exception(e.args)
 
     def getAllOrders(self):
-        queryset = Order.objects.all()
-        serializer = OrderSerializer(queryset, many=True)
-        data = serializer.data
-        return data
+        try:
+            data = cache.get("all_orders")
+            if data:
+                return data
+            queryset = Order.objects.all()
+            serializer = OrderSerializer(queryset, many=True)
+            data = serializer.data
+            cache.set("all_orders", data)
+            return data
+        except Exception as e:
+            raise Exception(e.args)
 
     def getAllCurrentOrders(self):
-        queryset = Order.objects.filter(delivered=False).all()
-        serializer = OrderSerializer(queryset, many=True)
-        return serializer.data
+        try:
+            data = cache.get("current_orders")
+            if data:
+                return data
+            queryset = Order.objects.filter(delivered=False).all()
+            serializer = OrderSerializer(queryset, many=True)
+            data = serializer.data
+            cache.set("current_orders", data)
+            return data
+        except Exception as e:
+            raise Exception(e.args)
 
     def update_order(self, order_id, updated_data):
         order_instance = Order.objects.get(order_id=order_id)
         serializer = OrderSerializer(instance=order_instance, data=updated_data)
         serializer.save()
-        return serializer.data
+        data = serializer.data
+        cached_order = cache.get("order_{order_id}")
+        if cached_order:
+            cache.delete("order_{order_id}")
+            cache.set("order_{order_id}", data)
+        return data
 
     def deleteOrder(self, order_id):
         try:
+            cached_order = cache.get("order_{order_id}")
+            if cached_order:
+                cache.delete("order_{order_id}")
             Deleted = Order.objects.get(order_id=order_id)
-
             deleted_count, _ = Deleted.delete()
             if deleted_count > 0:
                 return True
